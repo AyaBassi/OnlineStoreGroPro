f28990e19907348f0e8c55d52e1d55070193169c
aya bassi <pkarbassi@hotmail.co.uk>
2021-05-05 13:38:41 +0100

    First commit

---
+++ backendserver/data.js
@@ -0,0 +1,78 @@
+const data = {
+    products:[
+        {
+            _id:'1',
+            name:'Magners',
+            category:'Fruits',
+            image:'/images/p1.jpg',
+            price: 120,
+            countInStock:10,
+            brand: 'Spain',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'High quality product'
+        },
+        {
+            _id:'2',
+            name:'Apple',
+            category:'Fruits',
+            image:'/images/p2.jpg',
+            price: 100,
+            countInStock:15,
+            brand: 'Spain',
+            rating: 4.0,
+            numReviews: 10,
+            description: 'High quality product'
+        },
+        {
+            _id:'3',
+            name:'Large Oranges',
+            category:'Fruits',
+            image:'/images/p3.jpg',
+            price: 220,
+            countInStock:0,
+            brand: 'Spain',
+            rating: 4.8,
+            numReviews: 17,
+            description: 'High quality product'
+        },
+        {
+            _id:'4',
+            name:'Strawberries',
+            category:'Fruits',
+            image:'/images/p4.jpg',
+            price: 78,
+            countInStock:20,
+            brand: 'Spain',
+            rating: 4.5,
+            numReviews: 14,
+            description: 'High quality product'
+        },
+        {
+            _id:'5',
+            name:'Banana',
+            category:'Fruits',
+            image:'/images/p5.jpg',
+            price: 65,
+            countInStock:6,
+            brand: 'Puma',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'High quality product'
+        },
+        {
+            _id:'6',
+            name:'Canteloupe Melon',
+            category:'Fruits',
+            image:'/images/p6.jpg',
+            price: 139,
+            countInStock:13,
+            brand: 'Spain',
+            rating: 4.5,
+            numReviews: 15,
+            description: 'High quality product'
+        },
+    ]
+}
+
+export default data;
