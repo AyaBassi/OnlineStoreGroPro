efe02c6c23c7af38ea76efb49752a85f57bb1cac
aya bassi <pkarbassi@hotmail.co.uk>
2021-05-07 13:37:18 +0100

    Connect to mongo DB, create database create users collection, admin user, normal user sample.

--- backendserver/data.js
+++ backendserver/data.js
@@ -1,4 +1,19 @@
+import bcrypt from 'bcryptjs';
 const data = {
+    users:[
+        {
+            name:'Payam',
+            email:'admin@payam.com',
+            password: bcrypt.hashSync("1234",8),
+            isAdmin:true,
+        },
+        {
+            name:'Alex',
+            email:'alex@example.com',
+            password: bcrypt.hashSync("1234",8),
+            isAdmin:false,
+        }
+    ],
     products:[
         {
             _id:'1',
