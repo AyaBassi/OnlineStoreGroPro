b54dd19eba29f3d978b70825da82eb3a9e38395b
aya bassi <pkarbassi@hotmail.co.uk>
2021-05-11 00:19:42 +0100

    part of 31, Add successPay to useEffect options array

--- react_frontend/src/screens/OrderScreen.js
+++ react_frontend/src/screens/OrderScreen.js
@@ -46,7 +46,7 @@ function OrderScreen(props) {
         }
       }
     }
-  }, [dispatch, order, orderId, sdkReady]);
+  }, [dispatch, order, orderId, sdkReady, successPay]);
 
   const successPaymentHandler = (paymentResult) => {
     dispatch(payOrder(order, paymentResult));
