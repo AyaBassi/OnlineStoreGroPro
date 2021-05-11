2ce6f1fed9f4def5e3f551012b9ccb26eb4a81da
aya bassi <pkarbassi@hotmail.co.uk>
2021-05-12 00:53:13 +0100

    34.Implement update user  profile, there is a minor issue, for example if the name is changed and updated, it shows the updated version on the name field however if you go to home screen for example and return to profile screen again it would show the old one. I fixed this issue by taking out e.preventDefault(), to let user profile page refresh which seem to solve the issue.

--- react_frontend/src/screens/ProfileScreen.js
+++ react_frontend/src/screens/ProfileScreen.js
@@ -1,23 +1,45 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { detailsUser } from "../actions/userActions";
+import { detailsUser, updateUserProfile } from "../actions/userActions";
 import LoadingBox from "../components/LoadingBox";
 import MessageBox from "../components/MessageBox";
+import { USER_UPDATE__PROFILE_RESET } from "../constants/userConstants";
 
 function ProfileScreen() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
   const userSignin = useSelector((state) => state.userSignin);
   const { userInfo } = userSignin;
   const userDetails = useSelector((state) => state.userDetails);
   const { loading, error, user } = userDetails;
 
+  const userUpdateProfile = useSelector((state) => state.userUpdateProfile);
+  const {
+    success: successUpdate,
+    error: errorUpdate,
+    loading: loadingUpdate,
+  } = userUpdateProfile;
+
   const dispatch = useDispatch();
-  
   useEffect(() => {
-    dispatch(detailsUser(userInfo.id));
-  }, [dispatch, userInfo.id]);
+    dispatch({ type: USER_UPDATE__PROFILE_RESET });
+    if (!user) {
+      dispatch(detailsUser(userInfo._id));
+    } else {
+      setName(user.name);
+      setEmail(user.email);
+    }
+  }, [dispatch, userInfo._id, user]);
   const submitHandler = (e) => {
     e.preventDefault();
-    
+    if (password !== confirmPassword) {
+      alert("Passwords do not match!");
+    } else {
+      dispatch(updateUserProfile({ userId: user._id, name, email, password }));
+    }
   };
   return (
     <div>
@@ -31,13 +53,23 @@ function ProfileScreen() {
           <MessageBox variant="danger">{error}</MessageBox>
         ) : (
           <>
+            {loadingUpdate && <LoadingBox></LoadingBox>}
+            {errorUpdate && (
+              <MessageBox variant="danger">{errorUpdate}</MessageBox>
+            )}
+            {successUpdate && (
+              <MessageBox variant="success">
+                Profile Updated Successfully
+              </MessageBox>
+            )}
             <div>
               <label htmlFor="name">Name</label>
               <input
                 id="name"
                 type="text"
                 placeholder="Enter name"
-                value={user.name}
+                value={name}
+                onChange={(e) => setName(e.target.value)}
               ></input>
             </div>
             <div>
@@ -46,7 +78,8 @@ function ProfileScreen() {
                 id="email"
                 type="email"
                 placeholder="Enter email"
-                value={user.email}
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
               ></input>
             </div>
             <div>
@@ -55,6 +88,7 @@ function ProfileScreen() {
                 id="password"
                 type="password"
                 placeholder="Enter password"
+                onChange={(e) => setPassword(e.target.value)}
               ></input>
             </div>
             <div>
@@ -63,6 +97,7 @@ function ProfileScreen() {
                 id="confirmPassword"
                 type="password"
                 placeholder="Enter password again"
+                onChange={(e) => setConfirmPassword(e.target.value)}
               ></input>
             </div>
             <div>
