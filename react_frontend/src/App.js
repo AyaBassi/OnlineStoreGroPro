29927147857b5c8c84850469959fb8a3d5e02f72
aya bassi <pkarbassi@hotmail.co.uk>
2021-05-11 23:20:29 +0100

    33.Implement Display user profile

--- react_frontend/src/App.js
+++ react_frontend/src/App.js
@@ -12,6 +12,7 @@ import PaymentMethodScreen from "./screens/PaymentMethodScreen";
 import PlaceOrderScreens from "./screens/PlaceOrderScreens";
 import OrderScreen from "./screens/OrderScreen";
 import OrderHistoryScreen from "./screens/OrderHistoryScreen";
+import ProfileScreen from "./screens/ProfileScreen";
 
 function App() {
   const cart = useSelector((state) => state.cart);
@@ -48,6 +49,9 @@ function App() {
                 </Link>
                 <ul className="dropdown-content">
                   <li>
+                    <Link to="/profile">User Profile</Link>
+                  </li>
+                  <li>
                     <Link to="/orderhistory">Order History</Link>
                   </li>
                   <Link to="#signout" onClick={signoutHandler}>
@@ -70,6 +74,7 @@ function App() {
           <Route path="/placeorder" component={PlaceOrderScreens}></Route>
           <Route path="/order/:id" component={OrderScreen}></Route>
           <Route path="/orderhistory" component={OrderHistoryScreen}></Route>
+          <Route path="/profile" component={ProfileScreen}></Route>
           <Route path="/" component={HomeScreen} exact></Route>
         </main>
         <footer className="row center">All rights reserved</footer>
