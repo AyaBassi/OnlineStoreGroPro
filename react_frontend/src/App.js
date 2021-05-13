2e727a8884f1df2a653f92d7ae9bdc1aa47c9fb6
aya bassi <pkarbassi@hotmail.co.uk>
2021-05-13 23:11:54 +0100

    46. Implement list user screen
    1. build api for list users,2. Create userList Screen, create order details constant , action, and reducer

--- react_frontend/src/App.js
+++ react_frontend/src/App.js
@@ -18,6 +18,7 @@ import AdminRoute from "./components/AdminRoute";
 import ProductListScreen from "./screens/ProductListScreen";
 import ProductEditScreen from "./screens/ProductEditScreen";
 import OrderListScreen from "./screens/OrderListScreen";
+import UserListScreen from "./screens/UserListScreen";
 
 function App() {
   const cart = useSelector((state) => state.cart);
@@ -115,7 +116,11 @@ function App() {
             path="/productlist"
             component={ProductListScreen}
           ></AdminRoute>
-          <AdminRoute path="/orderlist" component={OrderListScreen}></AdminRoute>
+          <AdminRoute
+            path="/orderlist"
+            component={OrderListScreen}
+          ></AdminRoute>
+          <AdminRoute path="/userlist" component={UserListScreen}></AdminRoute>
           <Route path="/" component={HomeScreen} exact></Route>
         </main>
         <footer className="row center">All rights reserved</footer>
