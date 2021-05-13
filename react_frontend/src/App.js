88b30bc38af8b4a7b798c9c7cadc7afc6ee335d8
aya bassi <pkarbassi@hotmail.co.uk>
2021-05-13 12:20:13 +0100

    42. Create Order list screen for Admin
    1. Create order list api,2. create order list Screen, 3. Add reducer to store, 4.Show prodcuts on the screen.

--- react_frontend/src/App.js
+++ react_frontend/src/App.js
@@ -17,6 +17,7 @@ import PrivateRoute from "./components/PrivateRoute";
 import AdminRoute from "./components/AdminRoute";
 import ProductListScreen from "./screens/ProductListScreen";
 import ProductEditScreen from "./screens/ProductEditScreen";
+import OrderListScreen from "./screens/OrderListScreen";
 
 function App() {
   const cart = useSelector((state) => state.cart);
@@ -114,6 +115,7 @@ function App() {
             path="/productlist"
             component={ProductListScreen}
           ></AdminRoute>
+          <AdminRoute path="/orderlist" component={OrderListScreen}></AdminRoute>
           <Route path="/" component={HomeScreen} exact></Route>
         </main>
         <footer className="row center">All rights reserved</footer>
--- react_frontend/src/constants/userConstants.js
+++ react_frontend/src/constants/userConstants.js
@@ -1,5 +1,3 @@
-import expressAsyncHandler from "express-async-handler";
-
 export const USER_REGISTER_REQUEST = "USER_REGISTER_REQUEST";
 export const USER_REGISTER_SUCCESS = "USER_REGISTER_SUCCESS";
 export const USER_REGISTER_FAIL = "USER_REGISTER_FAIL";
